b8ebcf73110c27be4d88b3aac2cfeea979ba78a6
maharoof <mahafoosraheem123@gmail.com>
2026-07-16 16:16:36 +0530

    Update testAdd method to check new values

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,12 +1,12 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
     @Test
-    public void testAdd() {
-        assertEquals(5, App.add(2, 3));
+    void testAdd() {
+        assertEquals(10, App.add(4, 6));
     }
 }
